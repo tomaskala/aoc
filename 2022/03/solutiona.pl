7f520e8a5ce0de6e0e0f28806951f6b126acd86f
Tomas Kala <me@tomaskala.com>
2022-12-03 12:08:20 +0100

    Add day 3, part A

---
+++ 2022/03/solutiona.pl
@@ -0,0 +1,43 @@
+solve(P) :-
+  open(P,read,F),
+  process_lines(F,S),
+  close(F), !,
+  writeln(S).
+
+process_lines(F,S) :-
+  process_lines(F,S,0).
+process_lines(F,A,A) :-
+  at_end_of_stream(F).
+process_lines(F,S,A) :-
+  \+ at_end_of_stream(F),
+  read_line_to_codes(F,C),
+  ascii_to_alphabet(C,V),
+  find_item(V,X),
+  N is A+X,
+  process_lines(F,S,N).
+
+ascii_to_alphabet([],[]).
+ascii_to_alphabet([H|T],[X|Y]) :-
+  H>=65,
+  H=<90,
+  X is H-65+27,
+  ascii_to_alphabet(T,Y).
+ascii_to_alphabet([H|T],[X|Y]) :-
+  H>=97,
+  H=<122,
+  X is H-96,
+  ascii_to_alphabet(T,Y).
+
+find_item(L,C) :-
+  length(L,N),
+  M is N mod 2,
+  M=:=0,
+  H is N//2,
+  partition(L,H,F,S),
+  member(C,F),
+  member(C,S).
+
+partition(L,0,[],L).
+partition([H|T],N,[H|F],S) :-
+  M is N-1,
+  partition(T,M,F,S).
