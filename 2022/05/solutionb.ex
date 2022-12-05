03cbebbb6c048a73c061523cfbd33ece6b89bb94
Tomas Kala <me@tomaskala.com>
2022-12-05 23:58:15 +0100

    Add day 5, part B

---
+++ 2022/05/solutionb.ex
@@ -0,0 +1,39 @@
+defmodule SolutionB do
+  def read_stacks do
+    IO.stream(:stdio, :line)
+      |> Enum.take_while(fn line -> line != "\n" end)
+      |> Enum.reduce(%{}, fn line, stacks -> String.codepoints(line)
+        |> Enum.chunk_every(4)
+        |> Enum.with_index
+        |> Enum.reduce(stacks, fn {crate, n}, acc ->
+          case crate do
+            ["[", char, "]", _] ->
+              Map.update(acc, n + 1, [char], fn x -> [char|x] end)
+            _ -> acc
+          end
+        end)
+      end)
+      |> Enum.map(fn {k, v} -> {k, Enum.reverse(v)} end)
+      |> Map.new
+  end
+
+  def stack_crates(stacks) do
+    IO.stream(:stdio, :line) |> Enum.reduce(stacks, &move(&1, &2))
+  end
+
+  defp move(line, stacks) do
+    [_, n, _, s, _, d] = String.split(line, " ") |> Enum.map(&String.trim(&1))
+    num = String.to_integer(n)
+    src = String.to_integer(s)
+    dst = String.to_integer(d)
+    {init, tail} = Enum.split(stacks[src], num)
+    Map.put(stacks, src, tail) |> Map.update!(dst, fn x -> init ++ x end)
+  end
+end
+
+SolutionB.read_stacks
+  |> SolutionB.stack_crates
+  |> Map.to_list
+  |> Enum.sort(fn {k1, _}, {k2, _} -> k1 <= k2 end)
+  |> Enum.map(fn {_, v} -> hd(v) end)
+  |> IO.puts
