367485ccd8a2b69122a8137b8d2af69c9a2f7de2
Tomas Kala <me@tomaskala.com>
2022-12-06 21:22:04 +0100

    Add day 6, part A

---
+++ 2022/06/solutiona.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+#define WINDOW_SIZE 4
+
+int main()
+{
+  std::string line;
+  std::getline(std::cin, line);
+
+  if (line.length() < WINDOW_SIZE) {
+    std::cout << "The line must be at least " << WINDOW_SIZE
+      << " characters long" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  auto it = line.begin() + WINDOW_SIZE;
+  std::unordered_set<char> window(it - WINDOW_SIZE, it);
+
+  for (; it != line.end() && window.size() != WINDOW_SIZE; ++it) {
+    window.clear();
+    window.insert(it - WINDOW_SIZE, it);
+  }
+
+  std::cout << it - line.begin() - 1 << std::endl;
+  return EXIT_SUCCESS;
+}
