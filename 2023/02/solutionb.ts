5e9071a273733e5569bf4084e3394c9f2c87fc34
Tomas Kala <me@tomaskala.com>
2023-12-02 13:58:31 +0100

    Add day 2, part B

---
+++ 2023/02/solutionb.ts
@@ -0,0 +1,58 @@
+import { stdin } from "process";
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: stdin,
+});
+
+type Color = "red" | "green" | "blue";
+type Bag = Record<Color, number>;
+
+class Game {
+  private bags: ReadonlyArray<Bag>;
+
+  constructor(
+    public readonly gameId: number,
+    bags: ReadonlyArray<Bag>,
+  ) {
+    this.bags = bags;
+  }
+
+  power(): number {
+    return this.maxFor("red") * this.maxFor("green") * this.maxFor("blue");
+  }
+
+  private maxFor(color: Color): number {
+    return Math.max(...this.bags.map((bag) => bag[color]));
+  }
+
+  static parse(gameStr: string): Game {
+    const parts = gameStr.split(":");
+    const gameId = parseInt(parts[0].split(" ")[1]);
+    const bags = [];
+
+    for (const bagStr of parts[1].split(";")) {
+      const bag = { red: 0, green: 0, blue: 0 };
+
+      for (const colorStr of bagStr.split(",")) {
+        const [num, color] = colorStr.trim().split(" ");
+        bag[color as Color] = parseInt(num);
+      }
+
+      bags.push(bag);
+    }
+
+    return new Game(gameId, bags);
+  }
+}
+
+let sum = 0;
+
+rl.on("line", (line) => {
+  const game = Game.parse(line);
+  sum += game.power();
+});
+
+rl.on("close", () => {
+  console.log(sum);
+});
