a6a65222e57348f677be3b635331556c475c31e5
Tomas Kala <me@tomaskala.com>
2023-12-09 21:55:53 +0100

    Add day 9, part A

---
+++ 2023/09/solutiona.ts
@@ -0,0 +1,37 @@
+import { stdin } from "process";
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: stdin,
+});
+
+let sum = 0;
+
+function diff(sequence: Array<number>): void {
+  let prev = sequence[0];
+  let curr;
+
+  for (let i = 1; i < sequence.length; i++) {
+    curr = sequence[i];
+    sequence[i - 1] = sequence[i] - prev;
+    prev = curr;
+  }
+
+  sequence.pop();
+}
+
+rl.on("line", (line) => {
+  const sequence = line
+    .trim()
+    .split(" ")
+    .map((n) => parseInt(n));
+
+  while (!sequence.every((n) => n == 0)) {
+    sum += sequence[sequence.length - 1];
+    diff(sequence);
+  }
+});
+
+rl.on("close", () => {
+  console.log(sum);
+});
